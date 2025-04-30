b717ad0aef3a86026a1468a63b8e59591f0edf1f
Abhishek-kr12345 <abhishek.kumar@evervent.in>
2025-04-30 18:05:16 +0530

    Your message describing the change

--- controller/userController.js
+++ controller/userController.js
@@ -118,8 +118,9 @@ exports.Register = async (req, res) => {
 
 exports.login = async (req, res) => {
     const { email, password } = req.body;
+    if(!email || !password) return res.status(400).send("All fields are required")
     const user = await User.findOne({ email });
-
+    if(!user) return res.status(404).send("user not found")
     if (!user || !(await bcrypt.compare(password, user.password))) {
         return res.status(401).json({ message: "Invalid credentials" });
     }
--- server.js
+++ server.js
@@ -29,4 +29,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
     console.log("Hello world");
+    console.log("test");
 });
