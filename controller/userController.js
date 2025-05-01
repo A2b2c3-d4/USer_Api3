1d74e1ed5ff9bd8aef26575d60e19b6a64e10eec
Abhishek-kr12345 <abhishek.kumar@evervent.in>
2025-05-01 10:50:44 +0530

    change in login api

--- controller/userController.js
+++ controller/userController.js
@@ -118,9 +118,9 @@ exports.Register = async (req, res) => {
 
 exports.login = async (req, res) => {
     const { email, password } = req.body;
-    if(!email || !password) return res.status(400).send("All fields are required")
+    if(!email || !password) return res.status(400).send({message:"All fields are required"});
     const user = await User.findOne({ email });
-    if(!user) return res.status(404).send("user not found")
+    if(!user) return res.status(404).send({message: "user not found"})
     if (!user || !(await bcrypt.compare(password, user.password))) {
         return res.status(401).json({ message: "Invalid credentials" });
     }
